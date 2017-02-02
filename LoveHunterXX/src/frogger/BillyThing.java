d6cfe0b67f5a71f6903928e880f94819c7dbd3cb
Student6 <Student6@10.8.33.156>
2017-02-02 12:20:01 -0500

    created a billy file

---
+++ LoveHunterXX/src/frogger/BillyThing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package frogger;
+
+/**
+ * @author Billy
+ *
+ */
+public class BillyThing {
+
+	/**
+	 * 
+	 */
+	public BillyThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
