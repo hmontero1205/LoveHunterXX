d80800c612c1362d47d4cc91302334ba2e573cea
\Student 6 <Student 6@10.8.33.169>
2017-02-02 12:20:13 -0500

    created nockles file

---
+++ LoveHunterXX/src/frogger/HansTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package frogger;
+
+/**
+ * @author Hans
+ *
+ */
+public class HansTest {
+
+	/**
+	 * 
+	 */
+	public HansTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
