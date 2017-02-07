8b6d351da28532da3c4e6e26fc242b14e470cec9
Student6 <Student6@10.8.33.156>
2017-02-07 12:42:06 -0500

    screen

--- LoveHunterXX/src/quenchTheThirst/QttScreen.java
+++ LoveHunterXX/src/quenchTheThirst/QttScreen.java
@@ -1,9 +1,21 @@
 package quenchTheThirst;
 
-public class QttScreen {
+import java.util.ArrayList;
 
-	public QttScreen() {
+import gui.ClickableScreen;
+import gui.components.Visible;
+
+public class QttScreen extends ClickableScreen{
+
+	public QttScreen(int width, int height) {
 		// TODO Auto-generated constructor stub
+		super(width, height);
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
