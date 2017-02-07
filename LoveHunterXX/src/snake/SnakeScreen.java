7e0dffcc48b241c3543f0729610688d79b314c58
Student 6 <Student 6@10.8.33.159>
2017-02-07 12:53:12 -0500

    Created SnakeScreen -David.

---
+++ LoveHunterXX/src/snake/SnakeScreen.java
@@ -0,0 +1,44 @@
+package snake;
+/*
+ *  Created by David Medina.
+ * */
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import gui.Screen;
+import gui.components.Visible;
+
+public class SnakeScreen extends Screen implements KeyListener {
+
+	public SnakeScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
