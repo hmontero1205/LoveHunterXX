ac255146d1b4988680cc234bd19449d2bfe12a6e
Jia Ming <jma8774@bths.edu>
2017-02-09 12:54:05 -0500

    made 2 classes with nothing in it

---
+++ LoveHunterXX/src/frogger/AnimatedPlatform.java
@@ -0,0 +1,12 @@
+package frogger;
+
+import gui.components.AnimatedComponent;
+
+public class AnimatedPlatform extends AnimatedComponent {
+
+	public AnimatedPlatform(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ LoveHunterXX/src/frogger/Platform.java
@@ -0,0 +1,12 @@
+package frogger;
+
+import gui.components.MovingComponent;
+
+public class Platform extends MovingComponent {
+
+	public Platform(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+}
