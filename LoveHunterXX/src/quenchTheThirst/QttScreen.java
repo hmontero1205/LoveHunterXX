bd3d9242fdab66c88a9bb97d94e2b1d43c7bfb46
Student6 <Student6@10.8.33.156>
2017-02-10 11:59:17 -0500

    notes

--- LoveHunterXX/src/quenchTheThirst/QttScreen.java
+++ LoveHunterXX/src/quenchTheThirst/QttScreen.java
@@ -9,16 +9,23 @@ import gui.components.Visible;
 public class QttScreen extends Screen {
 	
 	private Graphic map;
+	private Player user;
 
 	public QttScreen(int width, int height) {
 		super(width, height);
 	}
-
+	/**
+	 * Screen needs to include the map, the ai(enemy) and the bottle
+	 * the enemy and bottle belong to a arraylist that keeps on changing
+	 * updates when bottle appear and disappear from screen
+	 * and updates when enemy spawn/get killed
+	 */
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		map = new Graphic(0,0,"resources/tempmap");
-		
+		user = new Player();
 		viewObjects.add(map);
+		viewObjects.add(user);
 	}
 
 }
