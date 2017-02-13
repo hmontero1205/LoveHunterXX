fe01a8b293bb6f0426725669de89c84cbf1231c9
Student 6 <Student 6@10.8.33.159>
2017-02-13 12:31:07 -0500

    asdasd

--- LoveHunterXX/src/snake/GuideScreen.java
+++ LoveHunterXX/src/snake/GuideScreen.java
@@ -7,10 +7,13 @@ import gui.ClickableScreen;
 import gui.components.Action;
 import gui.components.Button;
 import gui.components.Graphic;
+import gui.components.TextArea;
 import gui.components.Visible;
 
 public class GuideScreen extends ClickableScreen {
 
+	private final int WIDTH = 800;
+	private final int HEIGHT = 600;
 	public GuideScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -19,24 +22,26 @@ public class GuideScreen extends ClickableScreen {
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
-		
-		// Background image.jtyutyu
-		Graphic background = new Graphic(0, 0, "resources/ShoppingSpree.png");
+		// Background image.
+		Graphic background = new Graphic(0, 0, 800, 600, "resources/guidebackground.jpg");
 		
 		// button to be clicked to transition into the next screen.
-		Button button = new Button(400, 400, 100, 100, "Continue.", Color.BLACK, new Action() {
+		Button button = new Button((WIDTH/2) - (100/2), (HEIGHT/2) - (100/2), 100, 60, "Continue", Color.BLACK, new Action() {
 			
 			@Override
-			public void act() {
-				// TODO Auto-generated method stub
+			public void act() { // transition will occur here.
 				System.out.println("Let's continue!");
 				
 			}
 		});
 		
+		// text with all the instructions.
+		TextArea guideBox = new TextArea(100, 100, 200, 200, "Instructions:");
+		
 		// add items to the view list.
 		viewObjects.add(background);
 		viewObjects.add(button);
+		viewObjects.add(guideBox);
 		
 
 	}
