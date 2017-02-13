4347f5faf43b2c81db579d3e3f4083fd942cc9c8
Jia Ming <jma8774@bths.edu>
2017-02-13 13:15:03 -0500

    going to make an abstract class called MovingGameComponent

---
+++ LoveHunterXX/src/frogger/MovingGameComponent.java
@@ -0,0 +1,12 @@
+package frogger;
+
+import gui.components.AnimatedComponent;
+
+public abstract class MovingGameComponent extends AnimatedComponent {
+
+	public MovingGameComponent(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+}
