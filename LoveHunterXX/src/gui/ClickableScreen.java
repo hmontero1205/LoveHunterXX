e994f94a118b50b54f0916d8938bb8894bfafed5
Student6 <Student6@10.8.33.156>
2017-02-14 12:16:56 -0500

    screen change bug fix

--- LoveHunterXX/src/gui/ClickableScreen.java
+++ LoveHunterXX/src/gui/ClickableScreen.java
@@ -52,7 +52,7 @@ public abstract class ClickableScreen extends Screen implements MouseListener {
 	}
 
 	@Override
-	public void initObjects(ArrayList<Visible> viewObjects) {
+	public void initObjects(List<Visible> viewObjects) {
 		initAllObjects(viewObjects);
 		clickables = new ArrayList<Clickable>();
 		for(Visible v: viewObjects){
@@ -62,7 +62,7 @@ public abstract class ClickableScreen extends Screen implements MouseListener {
 		}
 	}
 	
-	public abstract void initAllObjects(ArrayList<Visible> viewObjects);
+	public abstract void initAllObjects(List<Visible> viewObjects);
 	
 	public MouseListener getMouseListener(){
 		return this;
