5124fbd6a106c94303df8727ac97fcb91a55490c
Student 7 <Student 7@10.8.33.168>
2017-02-14 12:13:49 -0500

    i am commiting this

---
+++ LoveHunterXX/src/snake/Present.java
@@ -0,0 +1,9 @@
+package snake;
+
+public class Present extends Interactable {
+
+	public Present() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- LoveHunterXX/src/snake/Snake.java
+++ LoveHunterXX/src/snake/Snake.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Snake {
 
 	private ArrayList<Interactable> presentList;
-	
 	private final static int UP = 0;
 	private final static int RIGHT = 0;
 	private final static int DOWN = 0;
@@ -19,7 +18,21 @@ public class Snake {
 		this.posX = 0;
 		this.posY = 0;
 		this.direction = DOWN;
+		presentList = new ArrayList<Interactable>();
 	}
 
+	private void addPresent(Present p){
+		presentList.add(p);
+	}
 	
+	private void removeLastPresent(){
+		presentList.remove(presentList.size()-1);
+	}
+	
+	private void moveCoors(){
+		for(int i = presentList.size()-1; i>0; i--){
+			
+		}
+		
+	}
 }
