5db3ced43cd01a8e7fcf067f835e50740fb3a1ed
Student6 <Student6@10.8.33.156>
2017-02-15 12:57:12 -0500

    projectile finish?

--- LoveHunterXX/src/quenchTheThirst/AlluringBottle.java
+++ LoveHunterXX/src/quenchTheThirst/AlluringBottle.java
@@ -2,20 +2,8 @@ package quenchTheThirst;
 
 public class AlluringBottle extends Projectile {
 
-	public AlluringBottle(int x, int y, int dir) {
-		super(x, y, 1, "resources/alluring");
-		
-	}
-
-	@Override
-	public void setVelocity(double v) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setAcceleration(double a) {
-		// TODO Auto-generated method stub
+	public AlluringBottle(int x, int y, String dir) {
+		super(x, y, 1, "resources/bottle.png", dir);
 		
 	}
 
--- LoveHunterXX/src/quenchTheThirst/ExplosiveBottle.java
+++ LoveHunterXX/src/quenchTheThirst/ExplosiveBottle.java
@@ -2,20 +2,8 @@ package quenchTheThirst;
 
 public class ExplosiveBottle extends Projectile{
 
-	public ExplosiveBottle(int x, int y, int dir) {
-		super(x, y, 1, "resources/explosive");
-	}
-
-	@Override
-	public void setVelocity(double v) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setAcceleration(double a) {
-		// TODO Auto-generated method stub
-		
+	public ExplosiveBottle(int x, int y, String dir) {
+		super(x, y, 1, "resources/bottle.png", dir);
 	}
 
 }
