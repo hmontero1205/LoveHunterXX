c2fef35baab51d6d33b914791724db09080fd7a8
micha <micha@Michael>
2017-02-15 19:56:10 -0500

    cart sprite.

--- LoveHunterXX/src/snake/Snake.java
+++ LoveHunterXX/src/snake/Snake.java
@@ -30,7 +30,7 @@ public class Snake extends MovingComponent{
 		this.direction = Direction.Down;
 		presentList = new ArrayList<Interactable>();
 		// last parameter is file path of image.
-		presentList.add(new SnakeHead(10,10,10,10, null)); // adding head.
+		presentList.add(new SnakeHead(10,10,10,10, "resources/cart.png")); // adding head.
 	}
 
 	private void addPresent(Present p){ // adding body parts.
