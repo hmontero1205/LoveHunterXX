2277ca435d262d1d966bbc633ae6242e998b5872
furryfaust <kevtehpro35@gmail.com>
2017-02-27 00:29:18 -0500

    Added animations to exploding and alluring bottle

--- LoveHunterXX/src/quenchTheThirst/AlluringBottle.java
+++ LoveHunterXX/src/quenchTheThirst/AlluringBottle.java
@@ -1,17 +1,29 @@
 package quenchTheThirst;
 
 public class AlluringBottle extends Projectile {
+	
+	private static String[] frames = new String[7];
+	static {
+		for (int i = 0; i < frames.length; i++) {
+			frames[i] = "resources/pool/pool" + i + ".png";
+		}
+	}
+	
 
 	public AlluringBottle(int x, int y, String dir) {
 		super(x, y, 0.05, "resources/bottle.png", dir);
+
 	}
 
 	@Override
 	public void hit() {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		for (int i = 0; i < 100; i++) {
+			loadImages(frames[i % frames.length], 0.5);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		
--- LoveHunterXX/src/quenchTheThirst/ExplosiveBottle.java
+++ LoveHunterXX/src/quenchTheThirst/ExplosiveBottle.java
@@ -1,13 +1,29 @@
 package quenchTheThirst;
 
 public class ExplosiveBottle extends Projectile {
+	
+	private static String[] frames = new String[47];
+	static {
+		for (int i = 0; i < frames.length; i++) {
+			frames[i] = "resources/explode/frame_" + i + "_delay-0.04s.gif";
+		}
+	}
 
 	public ExplosiveBottle(int x, int y, String dir) {
 		super(x, y, 0.03, "resources/bottle.png", dir);
 	}
 	
 	public void explode() {
-		System.out.println("BOOM");
+		for (int i = 0; i < frames.length; i++) {
+			loadImages(frames[i], 1.0);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		ShooterGame.shooterScreen.kill(this);
 	}
 
 	@Override
