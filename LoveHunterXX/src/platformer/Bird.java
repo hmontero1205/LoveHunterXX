017773b9c9111d13b0eaa5259927e5cbfa5d72c4
Daniel2 <dgoon9608>
2017-03-01 21:09:22 -0500

    T

---
+++ LoveHunterXX/src/platformer/Bird.java
@@ -0,0 +1,10 @@
+package platformer;
+
+public class Bird extends Obstacle {
+
+	public Bird(int x, int y, int w, int h, int vx, int vy, String imageLocation) {
+		super(x, y, w, h, vx, vy, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+}
