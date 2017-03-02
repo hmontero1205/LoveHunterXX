607a259dc1b4d6ef45bdaf23658b1234ad4751df
Jia Ming <jma8774@bths.edu>
2017-03-01 23:39:01 -0500

    asdsa

--- LoveHunterXX/src/frogger/Terrain.java
+++ LoveHunterXX/src/frogger/Terrain.java
@@ -60,18 +60,18 @@ public class Terrain extends Component implements Runnable {
 			case INVENTORY:
 				g.setColor(new Color(179, 179, 179));
 				g.fillRect(0, 0, getWidth(), getHeight());
-//				for(int i = 0; i < FroggerScreen.player.getInventory().size(); i ++) {
-//					ImageIcon im = new ImageIcon(FroggerScreen.player.getInventory().get(i).getImage());
-//					g.drawImage(im.getImage(), 10 + (i * 35), 10, 25, 25, null);
-//				}
-				ImageIcon im = new ImageIcon("resources/frogger/glove.png");
-				g.drawImage(im.getImage(), 10 + (0 * 35), 10, 25, 25, null);
-				im = new ImageIcon("resources/frogger/snorkel.png");
-				g.drawImage(im.getImage(), 10 + (1 * 35), 10, 25, 25, null);
-				g.drawImage(im.getImage(), 10 + (2 * 35), 10, 25, 25, null);
+				for(int i = 0; i < FroggerScreen.player.getInventory().size(); i ++) {
+					ImageIcon im = new ImageIcon(FroggerScreen.player.getInventory().get(i).getImage());
+					g.drawImage(im.getImage(), 10 + (i * 35), 10, 25, 25, null);
+				}
+//				ImageIcon im = new ImageIcon("resources/frogger/glove.png");
+//				g.drawImage(im.getImage(), 10 + (0 * 35), 10, 25, 25, null);
+//				im = new ImageIcon("resources/frogger/snorkel.png");
+//				g.drawImage(im.getImage(), 10 + (1 * 35), 10, 25, 25, null);
+//				g.drawImage(im.getImage(), 10 + (2 * 35), 10, 25, 25, null);
 				g.setColor(Color.RED);
-//				g.drawRect(7 + (FroggerScreen.player.getCurrentPowerUp() * 35) , 7, 31, 31); // weird numbers so that the selection box can center itself around the items
-				g.drawRect(7 + (0 * 35), 7, 31, 31);
+				if(FroggerScreen.player.getInventory().size() > 0) g.drawRect(7 + (FroggerScreen.player.getCurrentPowerUp() * 35) , 7, 31, 31); // weird numbers so that the selection box can center itself around the items
+//				g.drawRect(7 + (0 * 35), 7, 31, 31);
 
 				break;
 			default:
